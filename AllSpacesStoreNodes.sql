1d7d91e0f612a8df59623eb4531a3fe76fa8f0f4
hn-88 <hn-88@users.noreply.github.com>
2021-09-30 14:14:43 +0530

    removed superfluous version control lines

--- AllSpacesStoreNodes.sql
+++ AllSpacesStoreNodes.sql
@@ -1,8 +1,12 @@
---DECLARE url1 varchar DEFAULT 'https://www.srisathyasaividyavahini.org/alfresco/d/direct/workspace/SpacesStore/';
---DECLARE urlticket varchar DEFAULT '?ticket=TICKET_0bcfeffa9ea6995c53dd1ac99d1e4b371036f60a';
 -- from https://blog.dbi-services.com/alfresco-some-useful-database-queries/
 select concat('https://path/to/SpacesStore/', n.uuid,'/', np.string_value, '?ticket=TICKET_aaabbb__TheActualTicket_cccc')
 from alf_node_properties np
 left join alf_node n on np.node_id = n.id
 where n.store_id = 6
 and np.string_value like '%.%';
+-- This was also adding lines which had the following superfluous filenames (due to version control, which we did not need).
+-- So, removed the extra lines using the following:
+-- grep -v "/1.0?ticket=" dlist2.txt > dlist3.txt
+-- grep -v "/{http://www.alfresco.org/model/" dlist3.txt > dlist2.txt
+-- grep -v "000Z?ticket=" dlist2.txt > dlist3.txt
+-- This reduced the number of files nearly 3x.
